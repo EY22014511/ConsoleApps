b424d230d0c4110f1521f1f401a77a1bdaf7db75
EY22014511 <71990124+EY22014511@users.noreply.github.com>
2021-02-12 10:35:22 +0000

    Added the converter methods

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,78 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Converting distances
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Edrick Yapi version 0.1
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+        public const double METRES_IN_MILES = 1609.34;
+        private double miles;
+        private double feet;
+        private double metres;
 
+        public void Run()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateMetres();
+            CalculateFeet();
+            OutputMetres();
+            OutputFeet();
+            OutputMiles();
+        }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("     Convert Miles to Feet     ");
+            Console.WriteLine("         by Edrick Yapi        ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+        }
+
+        //Prompt for the user to enter the Miles
+        //Miles will be a double data type
+        private void InputMiles()
+        {
+            Console.WriteLine("Enter the number of miles: ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+
+        //method to calculate the miles inputted to feet
+        //feet will be a double data type
+        private void CalculateMetres()
+        {
+            metres = miles * METRES_IN_MILES;
+        }
+
+        private void OutputMetres()
+        {
+            Console.WriteLine(miles + " mile(s) is equivalent to " + metres + "m.");
+        }
+
+        //method to calculate the miles inputted to feet
+        //feet will be a double data type
+        private void CalculateFeet()
+        {
+            feet = miles * FEET_IN_MILES;
+        }
+
+        private void OutputFeet()
+        {
+            Console.WriteLine(miles + " mile(s) is equivalent to " + feet + " ft.");
+        }
+
+        private void OutputMiles()
+        {
+            Console.WriteLine(feet + " ft is equivalent to " + miles + " mi.");
+        }
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,6 +1,5 @@
-﻿using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
-using System;
+﻿using System;
+using ConsoleAppProject.App01;
 
 namespace ConsoleAppProject
 {
@@ -10,26 +9,19 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Edrick 11/02/2021
     /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            
+
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
 
-            // Using an extension method for each enumeration
-            Console.WriteLine("Using MyEnum Extension Method!\n");
-            Console.WriteLine("MyEnum Value = " + MyEnum.FirstValue);
-            Console.WriteLine("MyEnum Friendly Value = " + MyEnum.FirstValue.EnumValue());
-            Console.WriteLine();
-
-            // Using an extension method for any enumeration
-            StudentGrades grades = new StudentGrades();
-            grades.TestGradesEnumeration();
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
         }
     }
 }
