c4da6fccd0f2aba3d9d12b638c82ef6ab41c34b7
EY22014511 <71990124+EY22014511@users.noreply.github.com>
2021-03-23 01:21:09 +0000

    Testing

---
+++ ConsoleApps.Tests/App03 Unit Test.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App03;
+namespace ConsoleApps.Tests
+{
+    [TestClass]
+    public class TestStudentGrades
+    {
+        private readonly StudentGrades converter = new StudentGrades();
+
+        [TestMethod]
+        public void TestConvert0ToGradeF()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.F;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(0);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert39ToGradeF()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.F;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(39);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert40ToGradeD()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.D;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(40);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert49ToGradeD()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.D;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(49);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert50ToGradeC()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.C;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(50);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert59ToGradeC()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.C;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(59);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert60ToGradeB()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.B;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(60);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert69ToGradeB()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.B;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(69);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert70ToGradeA()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.A;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(70);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert100ToGradeA()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.A;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(100);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+    }
+}
